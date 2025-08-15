3bc96f6f8a187a9fa19f2aaaf4d43dbf6e95bf11
Jayraj Bhatia <157599061+JayB-31@users.noreply.github.com>
2025-08-15 21:35:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not browser
+
+console.log(3
+    +
+    3);
+// code readibility is important
+
+console.log("jayraj");
+
+let name = "jayraj" // string 
+let age = 22 // number
+let isloggedIn = false // boolean type
+
+
+// primitive datatypes
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/flase
+// null -> stand alone value
+// undefined ->  
+// symbol
+
+// object 
+
+console.log(typeof null); // and if you write undefined in typeof its value is undeifned 
+
+console.log(typeof undefined); // null value is a object 
