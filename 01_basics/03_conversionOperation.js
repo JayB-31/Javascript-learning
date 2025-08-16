3f8c49bf2d0fe4da8141db39e2f6c6410babe43c
Jayraj Bhatia <157599061+JayB-31@users.noreply.github.com>
2025-08-16 16:58:48 +0000

    operations more

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,8 +20,9 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" -> false
 // "jayraj" -> true
 
-// let someNumber = 33
-// let stringNumber = String(someNumber)
+let someNumber = 33
+
+let stringNumber = String(someNumber)
 // console.log(someNumber);
 // console.log(typeof stringNumber);
 
@@ -31,5 +32,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let value = 3
 let NegValue = -value
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(NegValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " jayraj"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 6);
+
+// console.log(true);
+
+let x = 3;
+const y = x++;
+console.log(x, y);
+
+let a = 3;
+const b = ++a;
+console.log(a, b);
+
