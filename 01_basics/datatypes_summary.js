850778a9235d618584ce34fe2be671795fa092b9
Jayraj Bhatia <157599061+JayB-31@users.noreply.github.com>
2025-08-20 21:33:22 +0000

    theory

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+// Two types of datatypes 1. primitive 2. non-primitive
+
+// 1. primitive datatypes:- 7 types: string, Number, Boolean, null, undefined, symbol-For special define, BigInt-For large value.
+
+// JavaScript is a dynamically typed language.
+// That means you don’t need to declare variable types explicitly. The type is determined automatically at runtime, and a variable’s type can change as the program runs. For example:
+// let x = 5;      // number
+// x = "hello";    // now it's a string
+// In contrast, statically typed languages (like Java, C, or TypeScript) require you to define variable types at compile time, and those types cannot change.
+
+// 2. Non-primitive also know as Refrence datatypes
+// Array, objects, Functions
