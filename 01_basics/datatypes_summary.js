36f75b5e0690b11b4b9e7508656349578cd112cd
Jayraj Bhatia <157599061+JayB-31@users.noreply.github.com>
2025-08-23 01:22:42 +0000

    datatypes summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -8,5 +8,33 @@
 // x = "hello";    // now it's a string
 // In contrast, statically typed languages (like Java, C, or TypeScript) require you to define variable types at compile time, and those types cannot change.
 
+// const score = 100
+// const socreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const andotherId = Symbol('123')
+
+// console.log(id === andotherId);
+
+
+// const bigNumber = 2525523623523532;
 // 2. Non-primitive also know as Refrence datatypes
 // Array, objects, Functions
+
+
+const heros = ["spiderman", "batman", "antman"];
+let myObject = {
+    name: "JAYRAJ",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof scoreValue);
