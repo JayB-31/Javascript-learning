bb8d81dae44b50e5ba80de6b0f5329cfc7338efc
Jayraj Bhatia <157599061+JayB-31@users.noreply.github.com>
2025-09-06 11:32:32 +0000

    project 1

--- 07_projects/1-colorChanger/chaiaurcode.js
+++ 07_projects/1-colorChanger/chaiaurcode.js
@@ -1,13 +1,29 @@
-const { JSDOM } = require("jsdom");
-const dom = new JSDOM(`
-<!DOCTYPE html>
-<body>
-  <button class="button">One</button>
-  <button class="button">Two</button>
-</body>
-`);
-
-const document = dom.window.document;
-
 const buttons = document.querySelectorAll('.button');
-console.log(buttons);
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    console.log(e.target);
+
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = 'grey';
+        break;
+
+      case 'white':
+        body.style.backgroundColor = 'white';
+        break;
+
+      case 'blue':
+        body.style.backgroundColor = 'blue';
+        break;
+
+      case 'yellow':
+        body.style.backgroundColor = 'yellow';
+        break;
+
+      default:
+        console.log('No matching color');
+    }
+  });
+});
